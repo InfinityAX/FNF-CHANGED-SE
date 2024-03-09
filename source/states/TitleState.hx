4b0039ff83ba4cbde64914e323ed17a119715dd0
Alain Xu <141105184+InfinityAX@users.noreply.github.com>
2024-03-08 21:52:49 -0500

    UPDATE 7
    - Fixed flashing state

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -156,11 +156,14 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		MusicBeatState.switchState(new ChartingState());
 		#else
-		if(FlxG.save.data.flashing == null && !FlashingState.leftState) {
+		if((FlxG.save.data.flashing == null || FlxG.save.data.flashing == true) && !FlashingState.leftState && !initialized)
+		{
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
 			MusicBeatState.switchState(new FlashingState());
-		} else {
+		}
+		else
+		{
 			if (initialized)
 				startIntro();
 			else
